e6e740abb06247b631693f4ca417eb4c3d51f880
Bo Zhao <BZhao7@slb.com>
2022-11-04 15:11:04 +0800

    feat: can change green when count less than 0

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -64,4 +64,18 @@ describe('CounterComponent', () => {
     const plusBtn = fixture.nativeElement.querySelector('[data-test="plusBtn"]')
     expect(plusBtn).toBeFalsy()
   });
+
+  it('should false when count number less than 0', () => {
+    // given
+    component.count = -1;
+    // when
+    expect(component.isLargerOrEqualsThanZero()).toBeFalse();
+  });
+
+  it('should false when count number greater than 10', () => {
+    // given
+    component.count = 11;
+    // when
+    expect(component.isLessOrEqualsTen()).toBeFalse();
+  });
 });
